914ba45ba6dfac6cd183c2d2b42fce2511339e0d
ramanand7 <ramanand9958654058@gmail.com>
2025-03-21 17:58:55 -0700

    data saved in the db

--- src/main/java/com/news/app/Controller/NewsController.java
+++ src/main/java/com/news/app/Controller/NewsController.java
@@ -1,11 +1,17 @@
 package com.news.app.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.news.app.Service.NewsService;
+import com.news.app.models.NewsResponse;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/news")
@@ -23,4 +29,10 @@ public class NewsController {
         }
         return "News fetched and stored successfully!";
     }
+
+    @GetMapping("/fetchByCategory")
+    public ResponseEntity<List<NewsResponse>> fetchCatergoryNews(@RequestParam String category) {
+        return newsService.fetchNewsByCategory(category);
+    }
+
 }
--- src/main/java/com/news/app/Service/NewsService.java
+++ src/main/java/com/news/app/Service/NewsService.java
@@ -1,7 +1,13 @@
 package com.news.app.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import com.news.app.Repository.NewsRepository;
@@ -16,6 +22,9 @@ public class NewsService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     private static final String API_KEY = "81d047c6f4eb4a1c9144471071db53e6";
     private static final String BASE_URL = "https://newsapi.org/v2/top-headlines?apiKey=" + API_KEY;
 
@@ -24,11 +33,23 @@ public class NewsService {
         NewsResponse response = restTemplate.getForObject(url, NewsResponse.class);
 
         if (response != null && "ok".equals(response.getStatus())) {
-            response.setId(category); // Set category as the ID for uniqueness
-            newsRepository.save(response);
+            response.setId(category);
+            mongoTemplate.save(response, category);
             System.out.println("Stored news for category: " + category);
         } else {
             System.out.println("Failed to fetch news for category: " + category);
         }
     }
+
+    public ResponseEntity<List<NewsResponse>> fetchNewsByCategory(String category) {
+        // Fetch data from the collection named after the category
+        List<NewsResponse> news = mongoTemplate.findAll(NewsResponse.class, category);
+
+        if (news.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(null); // Return 404 if no data is found
+        }
+        return ResponseEntity.ok(news); // Return 200 OK with the data
+    }
+
 }
